3a9591f9735eda338635d109a2ca61bc050fb69e
Robert <roberto88526@gmail.com>
2025-03-05 17:32:42 -0300

    support for the new minePower format.

--- src/services/player/mine.js
+++ src/services/player/mine.js
@@ -3,13 +3,13 @@ class PlayerMine {
         const diamondsRandom = (start, end) => Math.floor(Math.random() * (end - start) + start)
         let result = 0
 
-        if (minePower >= 9) {
+        if (minePower >= 90) {
             result = diamondsRandom(20, 31)
-        } else if (minePower >= 7) {
+        } else if (minePower >= 70) {
             result = diamondsRandom(15, 21)
-        } else if (minePower >= 5) {
+        } else if (minePower >= 50) {
             result = diamondsRandom(10, 16)
-        } else if (minePower >= 3) {
+        } else if (minePower >= 30) {
             result = diamondsRandom(5, 11)
         } else {
             result = diamondsRandom(1, 6)
