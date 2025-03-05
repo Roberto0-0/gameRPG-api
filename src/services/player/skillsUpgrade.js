20c58f68fc4619a2b04c14c266491a4b00b25ddb
Robert <roberto88526@gmail.com>
2025-03-05 10:43:14 -0300

    correction in the multiplication value.

--- src/services/player/skillsUpgrade.js
+++ src/services/player/skillsUpgrade.js
@@ -1,7 +1,7 @@
 class PlayerSkillsUpgrade {
     execute(skill) {
         const _skill = skill + 1
-        const price = Math.floor(Math.pow((_skill), 2) * 100)
+        const price = Math.floor(Math.pow((_skill), 2) * 10)
 
         return {
             skillUpdted: _skill, 
