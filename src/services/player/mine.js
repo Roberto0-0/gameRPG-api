bb7422bfa282d97e3ce5954290eb75ec8c656891
Robert <roberto88526@gmail.com>
2025-03-09 00:59:45 -0300

    more options to earn diamonds at every level.

--- src/services/player/mine.js
+++ src/services/player/mine.js
@@ -3,16 +3,37 @@ class PlayerMine {
         const diamondsRandom = (start, end) => Math.floor(Math.random() * (end - start) + start)
         let result = 0
 
-        if (minePower >= 90) {
-            result = diamondsRandom(20, 31)
-        } else if (minePower >= 70) {
-            result = diamondsRandom(15, 21)
-        } else if (minePower >= 50) {
-            result = diamondsRandom(10, 16)
-        } else if (minePower >= 30) {
-            result = diamondsRandom(5, 11)
-        } else {
-            result = diamondsRandom(1, 6)
+        switch (minePower) {
+            case 10:
+                result = diamondsRandom(1, 6)
+                break;
+            case 20:
+                result = diamondsRandom(5, 11)
+                break;
+            case 30:
+                result = diamondsRandom(10, 16)
+                break;
+            case 40:
+                result = diamondsRandom(15, 21)
+                break;
+            case 50:
+                result = diamondsRandom(20, 26)
+                break;
+            case 60:
+                result = diamondsRandom(25, 31)
+                break;
+            case 70:
+                result = diamondsRandom(30, 36)
+                break;
+            case 80:
+                result = diamondsRandom(35, 41)
+                break;
+            case 90:
+                result = diamondsRandom(40, 46)
+                break;
+            case 100:
+                result = diamondsRandom(45, 51)
+                break;
         }
 
         return result
