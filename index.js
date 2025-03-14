82f5a8d2d040800eb4d1104950e286802e71872a
Robert <roberto88526@gmail.com>
2025-03-14 19:06:05 -0300

    missionController added.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { GroupController } = require("./src/controllers/groupController")
 const { PlayerController } = require("./src/controllers/playerController")
+const { MissionController } = require("./src/controllers/missionController")
 
 class GameRPG {
     constructor(storagePath)  {
         this.storage = storagePath
         this.group = new GroupController(this.storage)
         this.player = new PlayerController(this.storage)
+        this.mission = new MissionController()
     }
 }
 
