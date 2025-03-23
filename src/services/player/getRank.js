cd8a50da52fa33d6e3f6c1e15ae003ba4c181896
Robert <roberto88526@gmail.com>
2025-03-22 22:17:56 -0300

    new way to get rank by level.

---
+++ src/services/player/getRank.js
@@ -0,0 +1,26 @@
+class PlayerGetRank {
+    execute(level) {
+        const ranks = [
+            "bronze",
+            "prata",
+            "ouro",
+            "mestre",
+            "lenda"
+        ]
+
+        const divisions = ["V", "IV", "III", "II", "I"];
+        const icons = ["🥉", "🥈", "🥇", "🏅", "🎖️"]
+
+        const rankIndex = Math.floor((level - 1) / 20)
+        const divisionProgress = (level - 1) % 20
+        const divisionIndex = Math.floor((divisionProgress + 1) / 5)
+
+        const currentRank = ranks[rankIndex];
+        const currentDivision = divisions[divisionIndex];
+        const currentIcon = icons[rankIndex]
+
+        return `${currentIcon} *${currentRank} ${currentDivision}*`;
+    }
+}
+
+module.exports = { PlayerGetRank }
