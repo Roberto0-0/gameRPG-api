9ef8476c7f4ac00f733456d4da155003f45978fa
Robert <roberto88526@gmail.com>
2025-03-27 01:54:28 -0300

    atomization and adjustments.

--- src/services/player/mine.js
+++ src/services/player/mine.js
@@ -1,42 +1,23 @@
 class PlayerMine {
-    execute(minePower) {
+    execute(powerLevel) {
+        const minePowerlevel = [
+            { level: 10, diamonds: 5 },
+            { level: 20, diamonds: 10 },
+            { level: 30, diamonds: 15 },
+            { level: 40, diamonds: 20 },
+            { level: 50, diamonds: 25 },
+            { level: 60, diamonds: 30 },
+            { level: 70, diamonds: 35 },
+            { level: 80, diamonds: 40 },
+            { level: 90, diamonds: 45 },
+            { level: 100, diamonds: 50 }
+        ]
+
         const diamondsRandom = (start, end) => Math.floor(Math.random() * (end - start) + start)
-        let result = 0
 
-        switch (minePower) {
-            case 10:
-                result = diamondsRandom(1, 6)
-                break;
-            case 20:
-                result = diamondsRandom(5, 11)
-                break;
-            case 30:
-                result = diamondsRandom(10, 16)
-                break;
-            case 40:
-                result = diamondsRandom(15, 21)
-                break;
-            case 50:
-                result = diamondsRandom(20, 26)
-                break;
-            case 60:
-                result = diamondsRandom(25, 31)
-                break;
-            case 70:
-                result = diamondsRandom(30, 36)
-                break;
-            case 80:
-                result = diamondsRandom(35, 41)
-                break;
-            case 90:
-                result = diamondsRandom(40, 46)
-                break;
-            case 100:
-                result = diamondsRandom(45, 51)
-                break;
-        }
+        const minePower = minePowerlevel.find(x => x.level === powerLevel)
 
-        return result
+        return diamondsRandom(1, (minePower.diamonds + 1))
     }
 }
 
