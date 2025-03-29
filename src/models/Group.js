896c76db6c96e06923356a9771ee30f524d4507f
Robert <roberto88526@gmail.com>
2025-03-29 09:33:24 -0300

    resetSeason removed.

--- src/models/Group.js
+++ src/models/Group.js
@@ -31,35 +31,6 @@ class Group {
 
         return date.getTime()
     }
-
-    resetSeason(_group) {
-        _group.players.sort((x, y) => {
-            return x.scoreSeason - y.scoreSeason
-        }).reverse()
-
-        let topPlayer = _group.palyers[0]
-
-        const newSeason = {
-            season: _group.currentSeason.season,
-            player: {
-                serialized: topPlayer.serialized,
-                name: topPlayer.name,
-                seasonScore: topPlayer.scoreSeason
-            },
-            createdAt: _group.currentSeason.createdAt
-        }
-
-        _group.nextSeason = this.setNextSeason()
-        _group.seasons.push(newSeason)
-        _group.currentSeason.season += 1
-        _group.currentSeason.createdAt = Date.now()
-
-        for (let player of _group.players) {
-            player.scoreSeason = 0
-        }
-
-        return
-    }
 }
 
 module.exports = { Group }
