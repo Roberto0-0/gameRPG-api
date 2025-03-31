3b40e8ccd5ccf5cd24be4f567bfc7dce1466f81d
Robert <roberto88526@gmail.com>
2025-03-31 08:34:11 -0300

    Now the reward has more options.

--- src/models/Mission.js
+++ src/models/Mission.js
@@ -9,6 +9,16 @@ class Mission {
         this.reward = this.info.reward
     }
 
+    setReward() {
+        const rewards = [
+            { name: "coins", icon: "🪙", value: 1000 },
+            { name: "keys", icon: "🗝️", value: 10 },
+            { name: "diamonds", icon: "💎", value: 100 }
+        ]
+
+        return rewards[Math.floor(Math.random() * (rewards.length - 0) + 0)]
+    }
+
     setMission(description, tag) {
         let result = {}
         const randomValue = (min, max) => Math.floor(Math.random() * (max - min) + min)
@@ -18,7 +28,7 @@ class Mission {
                 let diamonds = randomValue(50, 500)
                 result = {
                     description: description.replace("#quantity#", diamonds),
-                    reward: 1000,
+                    reward: this.setReward(),
                     objective: {
                         expected: diamonds,
                         currentValue: 0
@@ -29,7 +39,7 @@ class Mission {
                 let sales = randomValue(50, 500)
                 result = {
                     description: description.replace("#quantity#", sales),
-                    reward: 1000,
+                    reward: this.setReward(),
                     objective: {
                         expected: sales,
                         currentValue: 0
@@ -44,7 +54,7 @@ class Mission {
 
                 result = {
                     description: description,
-                    reward: 1000,
+                    reward: this.setReward(),
                     objective: {
                         expected: opponents,
                         currentValue: 0
@@ -54,7 +64,7 @@ class Mission {
             case "up":
                 result = {
                     description: description.replace("#quantity#", 1),
-                    reward: 1000,
+                    reward: this.setReward(),
                     objective: {
                         expected: 1,
                         currentValue: 0
@@ -67,7 +77,7 @@ class Mission {
                 description = description.replace("palpites", (duels == 1) ? "palpite" : "palpites")
                 result = {
                     description: description,
-                    reward: 1000,
+                    reward: this.setReward(),
                     objective: {
                         expected: duels,
                         currentValue: 0
