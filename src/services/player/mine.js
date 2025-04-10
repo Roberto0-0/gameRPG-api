200aa59f5d0aa840c136431fe1899061f1b17a67
Robert <roberto88526@gmail.com>
2025-04-10 14:46:52 -0300

    minDiamonds added.

--- src/services/player/mine.js
+++ src/services/player/mine.js
@@ -1,23 +1,23 @@
 class PlayerMine {
     execute(powerLevel) {
         const minePowerlevel = [
-            { level: 10, diamonds: 5 },
-            { level: 20, diamonds: 10 },
-            { level: 30, diamonds: 15 },
-            { level: 40, diamonds: 20 },
-            { level: 50, diamonds: 25 },
-            { level: 60, diamonds: 30 },
-            { level: 70, diamonds: 35 },
-            { level: 80, diamonds: 40 },
-            { level: 90, diamonds: 45 },
-            { level: 100, diamonds: 50 }
+            { level: 10, minDiamonds: 1, maxDiamonds: 5 },
+            { level: 20, minDiamonds: 1, maxDiamonds: 10 },
+            { level: 30, minDiamonds: 5, maxDiamonds: 15 },
+            { level: 40, minDiamonds: 5, maxDiamonds: 20 },
+            { level: 50, minDiamonds: 10, maxDiamonds: 25 },
+            { level: 60, minDiamonds: 15, maxDiamonds: 30 },
+            { level: 70, minDiamonds: 20, maxDiamonds: 35 },
+            { level: 80, minDiamonds: 25, maxDiamonds: 40 },
+            { level: 90, minDiamonds: 30, maxDiamonds: 45 },
+            { level: 100, maxDiamonds: 35, maxDiamonds: 50 }
         ]
 
-        const diamondsRandom = (start, end) => Math.floor(Math.random() * (end - start) + start)
+        const diamondsRandom = (x, y) => Math.floor(Math.random() * (y - x) + x)
 
         const minePower = minePowerlevel.find(x => x.level === powerLevel)
 
-        return diamondsRandom(1, (minePower.diamonds + 1))
+        return diamondsRandom(minePower.minDiamonds, (minePower.maxDiamonds + 1))
     }
 }
 
