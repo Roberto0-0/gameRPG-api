2a99c818b2a8abb855350b5053d7a9427bfdab65
Robert <roberto88526@gmail.com>
2025-04-20 18:08:33 -0300

    now calculating diamonds based on power level.

--- src/services/player/mine.js
+++ src/services/player/mine.js
@@ -1,23 +1,13 @@
 class PlayerMine {
-    execute(powerLevel) {
-        const minePowerlevel = [
-            { level: 10, minDiamonds: 1, maxDiamonds: 5 },
-            { level: 20, minDiamonds: 1, maxDiamonds: 10 },
-            { level: 30, minDiamonds: 5, maxDiamonds: 15 },
-            { level: 40, minDiamonds: 5, maxDiamonds: 20 },
-            { level: 50, minDiamonds: 10, maxDiamonds: 25 },
-            { level: 60, minDiamonds: 15, maxDiamonds: 30 },
-            { level: 70, minDiamonds: 20, maxDiamonds: 35 },
-            { level: 80, minDiamonds: 25, maxDiamonds: 40 },
-            { level: 90, minDiamonds: 30, maxDiamonds: 45 },
-            { level: 100, minDiamonds: 35, maxDiamonds: 50 }
-        ]
+    execute(minePowerLevel) {
+        const RNG = (x, y) => Math.floor(Math.random() * (y - x) + x)
 
-        const diamondsRandom = (x, y) => Math.floor(Math.random() * (y - x) + x)
+        const maximumDiamonds = Math.floor((minePowerLevel * 50) / 100)
 
-        const minePower = minePowerlevel.find(x => x.level === powerLevel)
+        let minimalDiamonds = (maximumDiamonds - 15)
+        if (minimalDiamonds <= 0) minimalDiamonds = 1
 
-        return diamondsRandom(minePower.minDiamonds, (minePower.maxDiamonds + 1))
+        return RNG((maximumDiamonds + 1), minimalDiamonds)
     }
 }
 
